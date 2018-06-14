036df60e09fe26dfe43028b1bce1f36919efec8a
Benedikt Fischer <bf@lottaleben.de>
2018-06-14 09:12:47 +0200

    tests replacement of plain strings in config.php

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Database Settings */
+define('DB_NAME', '%DBNAME%');
+define('DB_USER', '%DBUSER%');
+define('DB_PASSWORD', '%DBPW%');
+define('DB_HOST', '%DBHOST%');
+
+/* Custom Content Directory */
+define('CONTENT_DIR', '/content');
+define('WP_CONTENT_DIR', $rootDir . CONTENT_DIR);
+
+define('WP_ENV', '%ENV%');
